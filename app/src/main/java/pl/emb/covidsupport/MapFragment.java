20f4dc95bf1a2d15cd3a9bc86d859b4075873f9e
Hubert Gawlowski <224298@edu.p.lodz.pl>
2021-01-06 20:33:18 +0100

    Load gov.pl with map into webView to Poland fragment
    in the final version I want there only map, but I have to figure out how
    to do it

--- app/src/main/java/pl/emb/covidsupport/MapFragment.java
+++ app/src/main/java/pl/emb/covidsupport/MapFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MapFragment extends Fragment {
     WebView mWebView;
@@ -23,24 +24,28 @@ public class MapFragment extends Fragment {
         mWebView = root.findViewById(R.id.activity_webview);
         //enable javascript engine
         mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.loadUrl("https://flo.uri.sh/visualisation/4141169/embed?auto=1");
+        //mWebView.loadUrl("https://flo.uri.sh/visualisation/4141169/embed?auto=1");
+        // na razie ładujemy cała strona, koncowo chce pobrac sama mape ze strony i zamiescic ale na razie nie wiem jeszce jak
+        mWebView.loadUrl("https://www.gov.pl/web/koronawirus/wykaz-zarazen-koronawirusem-sars-cov-2");
         return root;
     }
 
+    // to odkomentowac, jesli chcemy pobrac mape z map.html (na razie jest tam szkic mapy, trzeba dodac js wykorzytywany na gov.pl, tego jeszcze nie wiem jak zrobic)
 //    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_map);
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+//                             @Nullable Bundle savedInstanceState) {
+//        root =  inflater.inflate(R.layout.fragment_map, container, false);
 //
-//        mWebView = (WebView) findViewById(R.id.activity_webview);
+//        mWebView = root.findViewById(R.id.activity_webview);
 //        //enable javascript engine
 //        mWebView.getSettings().setJavaScriptEnabled(true);
-//        mWebView.loadUrl("https://flo.uri.sh/visualisation/4141169/embed?auto=1");
+//        //mWebView.loadUrl("https://flo.uri.sh/visualisation/4141169/embed?auto=1");
 //        //mWebView.loadDataWithBaseURL(null, content, "text/html", "utf-8", null); // to jeśli mamy
 //        //                                      HTMla zadeklarowanego w Stringu w ramach tej klasy
-//        //mWebView.setWebViewClient(new WebViewClient());
+//        mWebView.setWebViewClient(new WebViewClient());
 //        //load webpage from assets
-//        //mWebView.loadUrl("map.html");
+//        mWebView.loadUrl("file:///android_asset/map.html");
+//        return root;
 //    }
 
     public void onClickRegionsBtn(View view) {
